633162b44fd11ea9879f95b9f879bf5e207406ee
Matthew Janes <mattjanes@gmail.com>
2026-06-25 07:28:06 -0400

    Testing Cleanup

--- GodotGAS/examples/demo_dummy.gd
+++
@@ -1,7 +0,0 @@
-class_name DemoDummy extends CharacterBody2D
-
-@onready var asc: AbilitySystemComponent = $AbilitySystemComponent
-@onready var passive_heal: GameplayAbility = $AbilitySystemComponent/GaPoisonRecovery
-
-func _ready() -> void:
-	asc.grant_ability(passive_heal)
